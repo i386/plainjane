2b15171cfba87eedffc693bd90710de6306dcbe5
James Dumay <james.w.dumay@gmail.com>
2013-02-19 20:55:57 +1100

    init version

--- Plain Jane/PJAppDelegate.m
+++ Plain Jane/PJAppDelegate.m
@@ -12,7 +12,40 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [NSEvent addGlobalMonitorForEventsMatchingMask:NSKeyDownMask handler:^(NSEvent *event) {
+        NSUInteger flags = [event modifierFlags];
+        int altDown = flags & NSCommandKeyMask;
+        
+        //Command + V
+        if (([event keyCode] == 9) && altDown) {
+            NSPasteboard *pboard = [NSPasteboard generalPasteboard];
+            
+            NSString* value = [pboard stringForType:NSStringPboardType];
+            
+            if (value) {
+                [pboard clearContents];
+                [pboard setString:value forType:NSStringPboardType];
+            }
+        };
+    }];
+    
+    [NSApp activateIgnoringOtherApps:YES];
 }
+
+-(void)applicationDidBecomeActive:(NSNotification *)notification
+{
+    [_window makeKeyAndOrderFront:nil];
+}
+
+- (IBAction)hideMainWindow:(id)sender
+{
+    [_window close];
+}
+
+- (IBAction)closeApp:(id)sender
+{
+    [[NSApplication sharedApplication] terminate:self];
+}
+
 
 @end
